34ae9b346e697cacae0f5e8dbdc9d63d2fcbb01e
MaGaroo <amoomajid99@gmail.com>
2018-03-30 19:58:15 +0430

    add person create view

--- menace/views.py
+++ menace/views.py
@@ -1,3 +1,8 @@
-from django.shortcuts import render
+from rest_framework import generics
 
 # Create your views here.
+from menace.serializers import PersonSerializer
+
+
+class PersonCreateView(generics.CreateAPIView):
+    serializer_class = PersonSerializer
